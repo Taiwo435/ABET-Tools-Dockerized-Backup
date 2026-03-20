5eb053846d31aafb9c211971a5cca2cde58755af
CUrbati <curbati@asu.edu>
2026-03-20 15:06:14 -0700

    Realized the sections where off, this is now fixed.

--- src/report_generation_api/report/report_builder.py
+++ src/report_generation_api/report/report_builder.py
@@ -12,6 +12,8 @@ from report.criteria import criterion_inst_support
 from report.criteria import criterion_program_edu_obj
 from report.criteria import criterion_students
 from report.criteria import program_criteria
+from report.criteria import general_criteria
+from report.criteria import criterion_student_outcomes
 
 from report.appendices import appendix_a
 from report.appendices import appendix_b
@@ -21,22 +23,24 @@ from report.appendices import appendix_e
 
 
 SECTIONS = [
-    ("Background", "background", background),
+    ("Background Information", "background", background),
+    ("General Criteria", "general_criteria", general_criteria),
     ("Criterion 1 - Students", "criterion_students", criterion_students),
     ("Criterion 2 - Program Educational Objectives", "criterion_program_edu_obj", criterion_program_edu_obj),
-    ("Criterion 3 - Continuous Improvement", "criterion_cont_improv", criterion_cont_improv),
-    ("Criterion 4 - Curriculum", "criterion_curriculum", criterion_curriculum),
-    ("Criterion 5 - Faculty", "criterion_faculty", criterion_faculty),
-    ("Criterion 6 - Facilities", "criterion_facilities", criterion_facilities),
-    ("Criterion 7 - Institutional Support", "criterion_inst_support", criterion_inst_support),
+    ("Criterion 3 - Student Outcomes", "criterion_student_outcomes", criterion_student_outcomes),
+    ("Criterion 4 - Continuous Improvement", "criterion_cont_improv", criterion_cont_improv),
+    ("Criterion 5 - Curriculum", "criterion_curriculum", criterion_curriculum),
+    ("Criterion 6 - Faculty", "criterion_faculty", criterion_faculty),
+    ("Criterion 7 - Facilities", "criterion_facilities", criterion_facilities),
+    ("Criterion 8 - Institutional Support", "criterion_inst_support", criterion_inst_support),
     ("Program Criteria", "program_criteria", program_criteria),
-    ("Accreditation Policies and Procedures", "accred_policies_procedures_man", accred_policies_procedures_man),
-    ("Submission Attestation to Compliance", "sub_attest_to_comp", sub_attest_to_comp),
+    ("Accreditation Policies and Procedures Manual", "accred_policies_procedures_man", accred_policies_procedures_man),
     ("Appendix A", "appendix_a", appendix_a),
     ("Appendix B", "appendix_b", appendix_b),
     ("Appendix C", "appendix_c", appendix_c),
     ("Appendix D", "appendix_d", appendix_d),
     ("Appendix E", "appendix_e", appendix_e),
+    ("Submission Attesting to Compliance", "sub_attest_to_comp", sub_attest_to_comp),
 ]
 
 
