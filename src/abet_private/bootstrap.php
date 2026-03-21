f67eef8abfc5375003b07bea89c1e2c8395fff9f
Danny Hoang <bajanboss132@gmail.com>
2026-03-20 19:52:31 -0700

    First valid migration

--- src/abet_private/bootstrap.php
+++ src/abet_private/bootstrap.php
@@ -2,7 +2,9 @@
 
 /**
  * REQUIRED TO BE IN THE ROOT FOLDER (with composer and friends)
- * the doctrine cli config
+ * 
+ * the doctrine config. Period.
+ * REQUIRE this file if you want to use the EntityManager!
  * 
  * run "composer doctrine" to get more info
  */
@@ -21,7 +23,11 @@ use Doctrine\DBAL\DriverManager;
 // config file (maybe make a config dir?)
 $config = new JsonFile('database/migrations.json'); // Or use one of the Doctrine\Migrations\Configuration\Configuration\* loaders
 
-$paths = [__DIR__.'/database/Entities'];
+$paths = [
+    __DIR__.'/database/entities',
+    __DIR__.'/database/yourmom'];
+
+echo $paths[0].'\n';
 $isDevMode = true;
 
 $ORMConfig = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
--- src/abet_private/cli-config.php
+++ src/abet_private/cli-config.php
@@ -7,43 +7,10 @@
  * run "composer doctrine" to get more info
  */
 
-require 'vendor/autoload.php';
-
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\ORMSetup;
 use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
 use Doctrine\Migrations\DependencyFactory;
-use Doctrine\Migrations\Configuration\Migration\JsonFile;
-use Doctrine\DBAL\DriverManager;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/../../docker");
-$dotenv->load();
-
-
-// config file (maybe make a config dir?)
-$config = new JsonFile('database/migrations.json'); // Or use one of the Doctrine\Migrations\Configuration\Configuration\* loaders
-
-$paths = [__DIR__.'/database/Entities'];
-$isDevMode = true;
-
-$ORMConfig = ORMSetup::createAttributeMetadataConfiguration($paths, $isDevMode);
-
-$connectionParams = [
-    'dbname' => getenv('MYSQL_DATABASE'),
-    'user' => getenv('MYSQL_USER'),
-    'password' => getenv('MYSQL_PASS'),
-    'host' => getenv('MYSQL_HOSTNAME'),
-    'driver' => 'pdo_mysql',
-];
-
-$connection = DriverManager::getConnection($connectionParams);
-
-
-// $connection = DriverManager::getConnection(
-//     ['driver' => 'pdo_mysql', 'memory' => true]);
-
-$entityManager = new EntityManager(
-    $connection, 
-    $ORMConfig);
+require_once 'vendor/autoload.php';
+require_once 'bootstrap.php';
 
 return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));  
