0116545f82dbd5a114f48c3d5e49a60f4044a52d
Danny Hoang <bajanboss132@gmail.com>
2026-03-29 13:32:33 -0700

    [WIP] Installed profiler

--- src/abet_private/src/Controller/SecurityController.php
+++ src/abet_private/src/Controller/SecurityController.php
@@ -6,12 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+
+// $response = new RedirectResponse('http://example.com/');
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        $request = Request::createFromGlobals();
+        // if ($request->isMethod('POST')) {
+        //     $response = new RedirectResponse('./home');
+        //     $response->send();
+        // }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
