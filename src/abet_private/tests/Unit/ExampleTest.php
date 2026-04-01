91ca5f194f7cc6a7bf7033a48e0f44741330bb92
Danny Hoang <bajanboss132@gmail.com>
2026-04-01 14:21:56 -0700

    Moved Pest tests to PHPUnit tests

--- src/abet_private/tests/Unit/ExampleTest.php
+++ src/abet_private/tests/Unit/ExampleTest.php
@@ -1,5 +1,11 @@
 <?php
+declare(strict_types=1);
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
+use PHPUnit\Framework\TestCase;
+
+final class ExampleTest extends TestCase {
+    public function testExample(): void {
+        $this->assertSame(true, true);
+    }
+}
+
