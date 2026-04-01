c35ce75f1c74558df008bb346343555b187dfee3
Danny Hoang <bajanboss132@gmail.com>
2026-04-01 14:16:29 -0700

    Added decorator to simplify tests + added /account/me test

--- src/test/test_navigation.py
+++ src/test/test_navigation.py
@@ -1,5 +1,5 @@
 """
-The test_login.py file contains Selenium-based tests for the login functionality of the ABET Tools web application. 
+The test_navigation (previously test_login.py) file contains Selenium-based tests for navigating through the UI to ensure the functionality of the ABET Tools web application. 
 This is meant to run with the dockerized Selenium Grid setup defined in the docker-compose.yml file.
 
 contains a template file for future tests .
@@ -10,12 +10,13 @@ implemented tests:
 import os
 from sys import exit
 from traceback import print_exc
+from functools import wraps
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
 
 from dotenv import load_dotenv
 import pytest
@@ -31,6 +32,10 @@ os.environ["PATH"] += os.pathsep + os.pathsep.join([
 ])
 
 def init_webdriver():
+    """
+    Initializes the webdriver in a completely fresh state
+    ensures that there is no leftover session state
+    """
     try:
         driver = webdriver.Remote(
             command_executor=f"http://localhost:{os.getenv('SELENIUM_PORT', '4444')}/wd/hub",
@@ -42,7 +47,46 @@ def init_webdriver():
         print_exc()
         exit(1)
 
-def test_login_invalid_credentials():
+
+def with_webdriver(func):
+    """
+    A decorator to shorten the lengths of tests by removing repeated webdriver setup
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        driver = init_webdriver()
+        try:
+            return func(driver, *args, **kwargs)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print_exc()
+        finally:
+            driver.quit()
+    return wrapper
+
+def template(): 
+    """
+    deprecated template for making new tests
+    """
+    driver = init_webdriver()
+    try:
+        driver.get(f"{WEBSITE_URL}")
+        driver.implicitly_wait(2)  # Wait for the page to load
+        print("Got the website")
+
+        # Add your test steps here
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print_exc()
+    finally:
+        driver.quit()
+
+
+def test_login_invalid_credentials(driver):
+    """
+    tests that users cannot simply access the website without having a valid session
+    """
     driver = init_webdriver()
     try:
         driver.get(f"{WEBSITE_URL}/login")
@@ -70,6 +114,9 @@ def test_login_invalid_credentials():
 
 @pytest.mark.order(1)
 def test_register_and_login_valid_credentials():
+    """
+    tests the register and login funcitonality for a valid login
+    """
     driver = init_webdriver()
     try:
         driver.get(f"{WEBSITE_URL}/register")
@@ -88,7 +135,7 @@ def test_register_and_login_valid_credentials():
         register_button.click()
         driver.implicitly_wait(2)  # Wait for the next page to load
 
-        sleep(2)
+        # sleep(2) 
 
         # check if there's a success message
         success_elements = driver.find_elements(By.CLASS_NAME, "success")
@@ -130,18 +177,43 @@ def test_register_and_login_valid_credentials():
         
 
 
-def template(): 
-    driver = init_webdriver()
-    try:
-        driver.get(f"{WEBSITE_URL}")
-        driver.implicitly_wait(2)  # Wait for the page to load
-        print("Got the website")
+@with_webdriver
+def test_navigation(driver): 
+    """
+    test using the new decorator i just made
+    should navigate to the homepage and some profile stuff
+    """
+    driver.get(f"{WEBSITE_URL}")
+    driver.implicitly_wait(2)  # Wait for the page to load
+    print("Got the website")
 
-        # Add your test steps here
+    driver.get(f"{WEBSITE_URL}/login")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print_exc()
-    finally:
-        driver.quit()
+    email_input = driver.find_element(By.ID, "email")
+    password_input = driver.find_element(By.ID, "password")
+    login_button = driver.find_element(By.CLASS_NAME, "btn-submit")
 
+    email_input.send_keys(EMAIL_ADDRESS)
+    password_input.send_keys(PASSWORD)
+    login_button.click()
+    driver.implicitly_wait(2)  # Wait for the next page to load
+
+    assert driver.current_url == f"{WEBSITE_URL}/home", "User not redirected to home after valid login"
+
+    # open profile dropdown
+    profile_button = driver.find_element(By.CLASS_NAME, "auth-btn");
+    profile_button.click()
+
+    # go to me
+    my_profile_link = driver.find_element(By.XPATH, "/html/body/header/div[2]/div/div/div/div/a");
+    my_profile_link.click()
+
+    assert driver.current_url == f"{WEBSITE_URL}/account/me", "User not at /account/me after button pressed"
+
+    back_to_dashboard_link = driver.find_element(By.XPATH, "/html/body/div/div[1]/div[1]/a");
+    back_to_dashboard_link.click()
+
+    assert driver.current_url == f"{WEBSITE_URL}/home", "User not at /home after navigation back pressed"
+
+
+    # Add your test steps here
