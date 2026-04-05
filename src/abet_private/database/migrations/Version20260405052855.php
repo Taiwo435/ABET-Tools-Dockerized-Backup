8cc4fa3fc723f0219ed00c8e237717b0b27e005d
Danny Hoang <bajanboss132@gmail.com>
2026-04-05 09:13:52 -0700

    skip migration if index already exists

--- src/abet_private/database/migrations/Version20260405052855.php
+++ src/abet_private/database/migrations/Version20260405052855.php
@@ -6,6 +6,7 @@ namespace Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Services;
 
 
 final class Version20260405052855 extends AbstractMigration
@@ -17,6 +18,11 @@ final class Version20260405052855 extends AbstractMigration
 
         public function up(Schema $schema): void
     {
+        $table = $schema->getTable('courses');
+
+        // Check if the index already exists
+        $this->skipIf($table->hasIndex('uniq_course_professor'), 'Skipped due to existing index');
+
         $this->addSql('ALTER TABLE courses ADD UNIQUE INDEX uniq_course_professor (course_id, professor_id)');
     }
 
