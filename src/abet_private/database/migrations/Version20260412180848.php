659ed4203a6beb8a491768b3985372f5ec2c7c74
Danny Hoang <bajanboss132@gmail.com>
2026-04-12 13:49:18 -0700

    migration fix to not have a syntax error

--- src/abet_private/database/migrations/Version20260412180848.php
+++ src/abet_private/database/migrations/Version20260412180848.php
@@ -31,26 +31,27 @@ final class Version20260412180848 extends AbstractMigration
         $this->addSql('ALTER TABLE course_syllabi ADD course_subject VARCHAR(50);');
 
         // new datatype for these guys
-        $this->addSql('ALTER TABLE course_syllabi SET instructor_name = NULL;');
-        $this->addSql('ALTER TABLE course_syllabi MODIFY instructor_name JSON;');
+        $this->addSql('ALTER TABLE course_syllabi DROP instructor_name = NULL;');
+        $this->addSql('ALTER TABLE course_syllabi ADD instructor_name JSON;');
 
-        $this->addSql('ALTER TABLE course_syllabi SET textbook = NULL;');
-        $this->addSql('ALTER TABLE course_syllabi MODIFY textbook JSON;');
+        $this->addSql('ALTER TABLE course_syllabi DROP textbook = NULL;');
+        $this->addSql('ALTER TABLE course_syllabi ADD textbook JSON;');
 
-        $this->addSql('ALTER TABLE course_syllabi SET specific_goals = NULL;');
-        $this->addSql('ALTER TABLE course_syllabi MODIFY specific_goals JSON;');
+        $this->addSql('ALTER TABLE course_syllabi DROP specific_goals = NULL;');
+        $this->addSql('ALTER TABLE course_syllabi ADD specific_goals JSON;');
 
-        $this->addSql('ALTER TABLE course_syllabi SET student_outcomes = NULL;');
-        $this->addSql('ALTER TABLE course_syllabi MODIFY student_outcomes JSON;');
+        $this->addSql('ALTER TABLE course_syllabi DROP student_outcomes = NULL;');
+        $this->addSql('ALTER TABLE course_syllabi ADD student_outcomes JSON;');
 
-        $this->addSql('ALTER TABLE course_syllabi SET topics_covered = NULL;');
-        $this->addSql('ALTER TABLE course_syllabi MODIFY topics_covered JSON;');
+        $this->addSql('ALTER TABLE course_syllabi DROP topics_covered = NULL;');
+        $this->addSql('ALTER TABLE course_syllabi ADD topics_covered JSON;');
 
     }
 
     public function down(Schema $schema): void
     {
         // programs table rollback
+        $this->throwIrreversibleMigrationException("Down not supported");
         $this->skipIf(!Services::doesColumnExist("programs", "program_year"), 'Skipping this migration.');
         $this->addSql('ALTER TABLE programs DROP INDEX unique_program;');
         $this->addSql('ALTER TABLE programs DROP COLUMN program_year;');
