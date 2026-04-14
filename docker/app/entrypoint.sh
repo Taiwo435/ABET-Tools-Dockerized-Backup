a6ad3c3a8394ec78540307f494caad4355f5b5ce
Danny Hoang <bajanboss132@gmail.com>
2026-04-13 22:39:59 -0700

    moved install scripts into entrypoint

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,7 +4,7 @@ set -a # automatically export all variables
 source "/home/docker/.env" > /dev/null
 set +a
 
-install -d -o www-data -g www-data -m 0766 "${ABET_PRIVATE_DIR}/report_jobs" 
-install -d -o www-data -g www-data -m 0766 "${ABET_PRIVATE_DIR}/var" 
+install -d -o www-data -g www-data -m 0777 "${ABET_PRIVATE_DIR}/report_jobs" 
+install -d -o www-data -g www-data -m 0777 "${ABET_PRIVATE_DIR}/var" 
 cp -rn /var/www/vendor "$ABET_PRIVATE_DIR/vendor"
 exec apache2-foreground
