29f230793fe69e82c5f595c1bc7f7d9566cfb038
Danny Hoang <bajanboss132@gmail.com>
2026-04-17 15:28:00 -0700

    feat(symfony) added routes for tool/assignmentsgrades/jobs

--- src/abet_private/src/Controller/AssignmentsGradesController.php
+++ src/abet_private/src/Controller/AssignmentsGradesController.php
@@ -19,7 +19,26 @@ final class AssignmentsGradesController extends AbstractController
         $parts = explode('@', (string)$user->getEmail());
         $asurite = $parts[0] ?? 'user';
         return $this->render('tools/assignments_grades/index.html.twig', [
-            'controller_name' => 'AssignmentsGradesController',
+            'asurite' => $asurite,
+        ]);
+    }
+
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/tool/assignmentsgrades/jobs', name: 'app_assignments_grades_jobs')]
+    public function jobs(#[CurrentUser] User $user) {
+        $parts = explode('@', (string)$user->getEmail());
+        $asurite = $parts[0] ?? 'user';
+        return $this->render('tools/assignments_grades/jobs.html.twig', [
+            'asurite' => $asurite,
+        ]);
+    }
+
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/tool/assignmentsgrades/course_select', name: 'app_assignments_grades_course_select')]
+    public function course_select(#[CurrentUser] User $user) {
+        $parts = explode('@', (string)$user->getEmail());
+        $asurite = $parts[0] ?? 'user';
+        return $this->render('tools/assignments_grades/course_select.html.twig', [
             'asurite' => $asurite,
         ]);
     }
