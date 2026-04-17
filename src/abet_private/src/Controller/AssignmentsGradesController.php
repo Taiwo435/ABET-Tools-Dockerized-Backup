22684ed760597c73f13ddf8f58fbcb8631aa0ebe
Danny Hoang <bajanboss132@gmail.com>
2026-04-17 14:29:14 -0700

    added template controller for AssignmentsGrades

---
+++ src/abet_private/src/Controller/AssignmentsGradesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use App\Entity\User;
+
+final class AssignmentsGradesController extends AbstractController
+{
+
+    
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/tool/assignmentsgrades', name: 'app_assignments_grades')]
+    public function tool_one(#[CurrentUser] User $user) {
+        $parts = explode('@', (string)$user->getEmail());
+        $asurite = $parts[0] ?? 'user';
+        return $this->render('tools/assignments_grades/index.html.twig', [
+            'controller_name' => 'AssignmentsGradesController',
+            'asurite' => $asurite,
+        ]);
+    }
+
+}
