4cba8d2154659100ada8cce1058ee115bbf8db2b
Danny Hoang <bajanboss132@gmail.com>
2026-04-17 18:46:59 -0700

    feat(symfony): Tool 1 form renders correctly and is handled by symfony

--- src/abet_private/src/Controller/AssignmentsGradesController.php
+++ src/abet_private/src/Controller/AssignmentsGradesController.php
@@ -18,13 +18,34 @@ final class AssignmentsGradesController extends AbstractController
     
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/tool/assignmentsgrades', name: 'app_assignments_grades')]
-    public function tool_one(#[CurrentUser] User $user) {
+    public function tool_one(#[CurrentUser] User $user, Request $request) {
+
+        //asurite
         $parts = explode('@', (string)$user->getEmail());
         $asurite = $parts[0] ?? 'user';
+
+        // creates a task object and initializes some data for this example
+        $task = new AccessTokenForm();
+        $task->setToken('');
+
+        $form = $this->createForm(AccessTokenType::class, $task);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            $task = $form->getData();
+
+            // ... perform some action, such as saving the task to the database
+
+            return $this->redirectToRoute('app_assignments_grades_jobs');
+        }
+
         return $this->render('tools/assignments_grades/index.html.twig', [
+            'form' => $form,
             'asurite' => $asurite,
         ]);
-    }
+    } 
 
     #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/tool/assignmentsgrades/jobs', name: 'app_assignments_grades_jobs')]
@@ -49,13 +70,17 @@ final class AssignmentsGradesController extends AbstractController
     #[Route('/tool/assignmentsgrades/testform', name: 'test_form')]
     public function new(Request $request, #[CurrentUser] User $user): Response
     {
+        //asurite
         $parts = explode('@', (string)$user->getEmail());
         $asurite = $parts[0] ?? 'user';
+
         // creates a task object and initializes some data for this example
         $task = new AccessTokenForm();
         $task->setToken('');
 
-        $form = $this->createForm(AccessTokenType::class, $task);
+        $form = $this->createForm(AccessTokenType::class, $task, [
+            'row_attr' => ['a' => 'b']
+        ]);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
