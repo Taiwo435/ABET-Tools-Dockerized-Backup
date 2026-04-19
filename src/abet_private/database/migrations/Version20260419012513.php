658f8b9e103e31a6301b314a55c7c67d534e0e48
Danny Hoang <bajanboss132@gmail.com>
2026-04-19 08:52:50 -0700

    fix(migration) added status column

--- src/abet_private/database/migrations/Version20260419012513.php
+++ src/abet_private/database/migrations/Version20260419012513.php
@@ -20,12 +20,14 @@ final class Version20260419012513 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE continuous_improvement MODIFY program_id INT NULL;');
+        $this->addSql("ALTER TABLE continuous_improvement ADD status ENUM('ongoing', 'completed') NULL;");
 
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE continuous_improvement MODIFY program_id INT NOT NULL;');
+        $this->addSql("ALTER TABLE continuous_improvement DROP status ENUM('ongoing', 'completed') NULL;");
 
     }
 }
