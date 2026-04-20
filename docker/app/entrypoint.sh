dde7ed075d6ae1510d2e9a8e8a61dfc4c8965909
Danny Hoang <bajanboss132@gmail.com>
2026-04-20 12:31:00 -0700

    fix(symfony-perms): apache server permissions now uses default perms

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -4,8 +4,8 @@ set -a # automatically export all variables
 source "/home/docker/.env" > /dev/null
 set +a
 
-install -d -o www-data -g www-data -m 0777 "${ABET_PRIVATE_DIR}/report_jobs" 
-install -d -o www-data -g www-data -m 0777 "${ABET_PRIVATE_DIR}/var" 
+install -d -o 1000 -g www-data -m 0777 "${ABET_PRIVATE_DIR}/report_jobs" 
+install -d -o 1000 -g www-data -m 0777 "${ABET_PRIVATE_DIR}/var" 
 cp -rn /var/www/vendor "$ABET_PRIVATE_DIR/vendor"
 if [ "$APP_ENV" = "dev" ]; then
     chown --recursive 1000:1000 vendor
