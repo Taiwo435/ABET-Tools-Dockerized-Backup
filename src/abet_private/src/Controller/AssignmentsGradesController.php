1f2e267e6a782343ae49162e6a59a4e1d3f51a75
Danny Hoang <bajanboss132@gmail.com>
2026-04-22 09:06:50 -0700

    feat(symfony) Tool 1 token validator finished

--- src/abet_private/src/Controller/AssignmentsGradesController.php
+++ src/abet_private/src/Controller/AssignmentsGradesController.php
@@ -30,24 +30,42 @@ final class AssignmentsGradesController extends AbstractController
         $parts = explode('@', (string)$user->getEmail());
         $asurite = $parts[0] ?? 'user';
 
-        // creates a task object and initializes some data for this example
+        /**
+         * This creates a form using these docs:
+         * @see https://symfony.com/doc/current/forms.html
+         */
+        // Initialize a AccessTokenForm that encapsulates the data returned
         $task = new AccessTokenForm();
         $task->setToken('');
 
+        // Use the form builder that we defined
         $form = $this->createForm(AccessTokenType::class, $task);
 
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             // $form->getData() holds the submitted values
             // but, the original `$task` variable has also been updated
             $task = $form->getData();
 
+            // use our API proxy to execute the task
             $response = $proxy->verifyToken($task->getToken());
 
-            var_dump($response->getStatusCode());
+            if ($response->getStatusCode() == 200) {
+                return $this->render('tools/assignments_grades/index.html.twig', [
+                    'form' => $form,
+                    'asurite' => $asurite,
+                    'form_success' => true
+                ]);
+            }
 
-            // return;
-            //return $this->redirectToRoute('app_assignments_grades_jobs');
+            // The other case: the form has errored.
+            // expects the response to be {'detail': error-msg}
+            return $this->render('tools/assignments_grades/index.html.twig', [
+                'form' => $form,
+                'asurite' => $asurite,
+                'form_error' => $response->toArray(false)['detail'],
+            ]);
         }
 
         return $this->render('tools/assignments_grades/index.html.twig', [
