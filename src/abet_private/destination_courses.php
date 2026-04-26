23e9ec0f45e50b09b26704a37aabb8cee40eb2a6
Danny Hoang <bajanboss132@gmail.com>
2026-04-26 13:40:37 -0700

    fix(config): reverted unintended config changes by someone

--- src/abet_private/destination_courses.php
+++ src/abet_private/destination_courses.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'dest_courses' => [
-        ['label' => 'CS', 'id' => '2242012102212'], 
-        ['label' => 'CSE', 'id' => '22401029'], 
+        ['label' => 'CS', 'id' => '240102'], 
+        ['label' => 'CSE', 'id' => '240102'], 
     ]
 ];
